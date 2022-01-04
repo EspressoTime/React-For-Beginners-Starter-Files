ec52fe5aa5b896db93d0388abe5816e550b1c884
EspressoTime <EspressoTime@users.noreply.github.com>
2022-01-03 22:39:02 -0800

    Render JSX example

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Store Picker Content</p>
+        return (
+            <React.Fragment>
+                <p>Store Picker</p>
+                {/* React Fragment allows for multiple tags without a div wrapper */}
+                <form className="store-selector">
+                    <h2>Select A Store</h2>
+                    <input type="text" required placeholder="Store Name" />
+                    <button type="submit">Visit Store &gt;</button>
+                </form>
+            </React.Fragment>
+
+        )
     }
 }
 
